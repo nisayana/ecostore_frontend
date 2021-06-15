6d9d571c8a1575482c4f6e7ac894df419a33be55
nisayana <nimaevasb@gmail.com>
2021-06-14 22:07:40 -0400

    styling

--- src/HomeComponents/HomeContainer.js
+++ src/HomeComponents/HomeContainer.js
@@ -9,11 +9,12 @@ class HomeContainer extends React.Component {
     // console.log("from Home cont", this.props)
     return(
       <div>
-        <h1 id="header-home">Eco Store</h1>
-        <img id="banner" height="300" src="https://res.cloudinary.com/dv588hi0a/image/upload/v1613493366/samples/Screen_Shot_2021-02-16_at_11.35.25_AM_ioujfg.png" alt="eco-store"></img>
-        {/* <video preload="none" autoplay="" muted={true} loop="" playsinline="true" src="https://video.wixstatic.com/video/b34ada_5cb4c7035b774337af2b74914d651ef3/360p/mp4/file.mp4"></video> */}
-        {/* <h2>“Yoga is the journey of the self, through the self, to the self.” -The Bhagavad Gita</h2> */}
-        {/* <CategoryContainer categories={this.props.categories} /> */}
+        <div class="home-img-container">
+          <img id="banner" height="300" src="https://res.cloudinary.com/dv588hi0a/image/upload/v1613493366/samples/Screen_Shot_2021-02-16_at_11.35.25_AM_ioujfg.png" alt="eco-store"></img>
+          <div class="slogan">
+            <h1>Choose a better tomorrow</h1>
+          </div>
+        </div>
         <MainContainer 
         items={this.props.items} 
         token = {this.props.token}
